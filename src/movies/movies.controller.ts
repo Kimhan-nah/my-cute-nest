d3e7713d0da3adceffc82e33ff30c74e8d7454ec
Kimhannah <skqkdldhf98@naver.com>
2023-04-07 16:10:03 +0900

    :sparkles: feat: Add search using Query decorator

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Post, Delete, Put, Patch } from '@nestjs/common';
+import { Controller, Get, Param, Post, Delete, Patch, Body, Query } from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -7,14 +7,21 @@ export class MoviesController {
 		return 'This will return all movies';
 	}
 
+	// movies/search?year=2000
+	// search가 get("/:id")보다 밑에 있으면 NestJS는 search를 id로 판단 -> 위에 위치해야 함
+	@Get("search")
+	search(@Query("year") searchingYear: string) {	// 가져오는 변수와 return 값을 확인할 수 있음
+		return `We are searching for a movie with a title: ${searchingYear}`;
+	}
+
 	@Get("/:id")
 	getOne(@Param("id") movieId: string) {
 		return `This will return one movie with the id ${movieId}`;
 	}
 
 	@Post()
-	create() {
-		return 'This will create a movie';
+	create(@Body() movieData) {
+		return movieData;
 	}
 
 	@Delete("/:id")
@@ -23,7 +30,11 @@ export class MoviesController {
 	}
 
 	@Patch('/:id')
-	path(@Param('id') movieId: string) {
-		return `This will patch a movie with the id ${movieId}`;
+	patch(@Param('id') movieId: string, @Body() updateData) {
+		return {
+			updatedMovie: movieId,
+			...updateData,
+		};
 	}
+
 }
