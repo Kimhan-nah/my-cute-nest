fd47e1115c03bff0657d9316c8fd1f2488ae430f
Kimhannah <skqkdldhf98@naver.com>
2023-04-08 20:14:25 +0900

    :green_heart: chore: Modify id type from string to number

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Param, Post, Delete, Patch, Body, Query } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { Movie } from './entities/movie.entity';
+import { CreateMovieDto } from './dto/create-movie.dto';
 
 @Controller('movies')
 export class MoviesController {
@@ -19,25 +20,24 @@ export class MoviesController {
 	// }
 
 	@Get("/:id")
-	getOne(@Param("id") movieId: string): Movie {
+	getOne(@Param("id") movieId: number): Movie {
 		return this.moviesService.getOne(movieId);
 		// return `This will return one movie with the id ${movieId}`;
 	}
 
 	@Post()
-	create(@Body() movieData) {
-		// return movieData;
+	create(@Body() movieData: CreateMovieDto) {
 		return this.moviesService.create(movieData);
 	}
 
 	@Delete("/:id")
-	remove(@Param('id') movieId: string) {
+	remove(@Param('id') movieId: number) {
 		// return `This will delete a movie with the id ${movieId}`;
 		return this.moviesService.deleteOne(movieId);
 	}
 
 	@Patch('/:id')
-	patch(@Param('id') movieId: string, @Body() updateData) {
+	patch(@Param('id') movieId: number, @Body() updateData) {
 		return this.moviesService.update(movieId, updateData);
 	}
 
